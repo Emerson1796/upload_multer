a26d922570bb01850777d9d103bbf76d392172c4
LAPTOP-BNDEII85\emers <emerson.teixeira@atmodigital.com.br>
2022-01-12 18:37:15 -0300

    projeto completo

---
+++ server.js
@@ -0,0 +1,34 @@
+// não esqueça de importar o módulo path!
+const express = require('express')
+	, app = express()
+	, multer = require('multer')
+	, path = require('path');
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`);
+//     }
+// });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+app.use(express.static('public'));
+
+// continua do mesma forma 
+app.post('/file/upload', upload.single('file'), 
+    (req, res) => res.send('<h2>Upload realizado com sucesso</h2>'));  
+
+app.listen(3000, () => console.log('App na porta 3000'));
